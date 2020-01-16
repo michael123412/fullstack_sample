b1f67a5b09b56a052995b4230209fc1b11c4d8df
michael123412 <41839877+michael123412@users.noreply.github.com>
2020-01-16 14:18:24 +0100

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,8 +1,10 @@
 //Install express server
+const compression = require('compression')
 const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(compression())
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/apps/fitness-app'));
